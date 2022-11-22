58f219ef3eca81328ebd0abe93a8e099763eff3c
angeltried <angeltried@icloud.com>
2022-11-21 20:04:25 -0600

    routes

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -8,28 +8,41 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      product_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate: {
+          isDecimal: true,
+        },
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+        validate: {
+          isNumeric: true,
+        },
+      },
+      category_id:{
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'category',
+          key: 'id'
+        }
+      },
     },
-    product_id:{
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'product',
-        key: 'id'
-      }
-    },
-    tag_id:{
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'tag',
-        key: 'id'
-      }
-    },
-  },
   {
     sequelize,
     timestamps: false,
